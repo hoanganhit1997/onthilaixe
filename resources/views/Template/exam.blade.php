94e9441884e22bc079938b0956ffafc3a18fe8b6
conghieutlvy <conghieutlvy@gmail.com>
2017-05-08 20:30:14 +0700

    fix boostrap

--- resources/views/Template/exam.blade.php
+++ resources/views/Template/exam.blade.php
@@ -6,8 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="http://www.layoutit.com/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+   <!-- <link href="http://www.layoutit.com/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">-->
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
   </head>
   <body>
@@ -555,9 +563,9 @@
 					</div>
 				</div> 
     <a class="left carousel-control" href="#carousel-1" data-slide="prev">
-    	<!--<span class="glyphicon glyphicon-chevron-left"></span>--></a> 
+    	span class="glyphicon glyphicon-chevron-left"></span></a> 
     <a class="right carousel-control" href="#carousel-1" data-slide="next">
-    	<!--<span class="glyphicon glyphicon-chevron-right"></span>--></a>
+    	<span class="glyphicon glyphicon-chevron-right"></span></a>
                 
   		</div>
                      <div class="row">
@@ -569,8 +577,8 @@
                     </div>
  </form>  
 
-
+<!--
     <script src="http://www.layoutit.com/js/jquery.min.js"></script>
     <script src="http://www.layoutit.com/js/bootstrap.min.js"></script>
-    <script src="http://www.layoutit.com/js/scripts.js"></script>
+    <script src="http://www.layoutit.com/js/scripts.js"></script>-->
 </html>
